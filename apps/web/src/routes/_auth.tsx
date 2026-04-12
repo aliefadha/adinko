b46f5bd059560be3c27bfa3a3839a8a0ad0bf408
aliefadha <88047810+aliefadha@users.noreply.github.com>
2026-04-12 08:13:13 +0700

    feat: move auth checking to layout

--- apps/web/src/routes/_auth.tsx
+++ apps/web/src/routes/_auth.tsx
@@ -1,7 +1,20 @@
-import { createFileRoute, Outlet } from '@tanstack/react-router'
+import { getUser } from '@/functions/get-user';
+import { createFileRoute, Outlet, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_auth')({
     component: AuthComponent,
+    beforeLoad: async () => {
+        const session = await getUser();
+        return { session };
+    },
+    loader: async ({ context }) => {
+        if (context.session) {
+            throw redirect({
+                to: "/admin/kategori",
+            });
+        }
+        return { session: context.session };
+    },
 })
 
 function AuthComponent() {
